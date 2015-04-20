e322daa02b87dd82e00054a68bd38ffb4c7ffb40
rigs147 <seanrigney@hotmail.co.uk>
2015-04-20 14:43:34 +0100

    initial commit

---
+++ js/services.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/* Services */
+
+landingApp.factory('deviceService', function ($window, $location, deviceType, localStorageService) {
+
+    return function () {
+
+        if (/Android|webOS|iPhone|iPad|iPod|PlayBook|BlackBerry|IEMobile|Opera Mini|SymbianOS/i.test(navigator.userAgent)) {
+            //Filter for mobile devices. This service runs at app start
+
+            if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+                deviceType.isIos = true;
+            }
+            else if (/Android/i.test(navigator.userAgent)) {
+                deviceType.isAndroid = true;
+            }
+
+            deviceType.isDesktop = false;
+
+            $location.path('/CountryPicker');
+        }
+        else {
+                //you are on a desktop
+
+            var cookie = localStorageService.get('aoCookie')
+
+            if (cookie != undefined) {
+
+                //Go to office of aoCookie value
+                $window.location.href = cookie;
+                $location.path('/Redirect');
+
+
+            } else {
+                $location.path('/CountryPicker');
+
+             }
+        }
+    }
+});
+
+deleteCookieApp.factory('deleteCookieService', function ($window, $rootScope, localStorageService) {
+
+    var cookie = localStorageService.get('aoCookie');
+
+
+    if (cookie != undefined) {
+        localStorageService.remove('aoCookie');
+        $rootScope.$digest();
+    }
+
+    $window.location.href = 'index.html';
+
+});
+
+
+
+
