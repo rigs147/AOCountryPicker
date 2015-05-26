043c7b80214b11eeab0111d1baad3a95dba4fd77
rigs147 <seanrigney@hotmail.co.uk>
2015-05-26 14:01:43 +0100

    test

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 /* Directives */
 
@@ -9,39 +9,39 @@
 //      elm.text(version);
 //    };
 //  }]);
-landingApp.directive('fileDownload', [function () {
-    return {
-        restrict: 'A',
-        replace: true,
-        template: '<button class="btn btn-default" data-ng-click="download()"><span class="glyphicon glyphicon-download"></span></button>',
-        controller: ['$rootScope', '$scope', '$element', '$attrs', 'dialogs', '$timeout', function ($rootScope, $scope, $element, $attrs, dialogs, $timeout) {
-            $scope.progress = 0;
-
-            function prepare(url) {
-                dialogs.wait("Please wait", "Your download starts in a few seconds.", $scope.progress);
-                fakeProgress();
-            }
-            function success(url) {
-                $rootScope.$broadcast('dialogs.wait.complete');
-            }
-            function error(response, url) {
-                dialogs.error("Couldn't process your download!");
-            }
-
-            function fakeProgress() {
-                $timeout(function () {
-                    if ($scope.progress < 95) {
-                        $scope.progress += (96 - $scope.progress) / 2;
-                        $rootScope.$broadcast('dialogs.wait.progress', { 'progress': $scope.progress });
-                        fakeProgress();
-                    }
-                }, 250);
-            }
-
-            $scope.download = function () {
-                $scope.progress = 0;
-                $.fileDownload($attrs.href, { prepareCallback: prepare, successCallback: success, failCallback: error });
-            }
-        }]
-    }
-}]);
+//landingApp.directive('fileDownload', [function () {
+//    return {
+//        restrict: 'A',
+//        replace: true,
+//        template: '<button class="btn btn-default" data-ng-click="download()"><span class="glyphicon glyphicon-download"></span></button>',
+//        controller: ['$rootScope', '$scope', '$element', '$attrs', 'dialogs', '$timeout', function ($rootScope, $scope, $element, $attrs, dialogs, $timeout) {
+//            $scope.progress = 0;
+//
+//            function prepare(url) {
+//                dialogs.wait("Please wait", "Your download starts in a few seconds.", $scope.progress);
+//                fakeProgress();
+//            }
+//            function success(url) {
+//                $rootScope.$broadcast('dialogs.wait.complete');
+//            }
+//            function error(response, url) {
+//                dialogs.error("Couldn't process your download!");
+//            }
+//
+//            function fakeProgress() {
+//                $timeout(function () {
+//                    if ($scope.progress < 95) {
+//                        $scope.progress += (96 - $scope.progress) / 2;
+//                        $rootScope.$broadcast('dialogs.wait.progress', { 'progress': $scope.progress });
+//                        fakeProgress();
+//                    }
+//                }, 250);
+//            }
+//
+//            $scope.download = function () {
+//                $scope.progress = 0;
+//                $.fileDownload($attrs.href, { prepareCallback: prepare, successCallback: success, failCallback: error });
+//            }
+//        }]
+//    }
+//}]);
